05ecafbe04216687ed1d55ae93df86fd6ecd8de2
manthanshevlkar <manthanshevlkar@localhost>
2025-02-21 19:10:47 +0530

    initial commit

---
+++ Ecommerce_array_list_file/Product.java
@@ -0,0 +1,8 @@
+package Ecommerce_array_list_file;
+
+public class Product {
+
+	public String productName;
+	public String productprice;
+	public String productQuantity;
+}
---
+++ Ecommerce_array_list_file/User.java
@@ -0,0 +1,6 @@
+package Ecommerce_array_list_file;
+
+public class User {
+	public String userName;
+	public String userMail;
+}
